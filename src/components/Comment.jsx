32d62b5225e0573458a977c111d3e4364481a1c7
Gabe-Desktop <gmattoslopes@gmail.com>
2025-01-16 10:14:23 -0400

    Criado a estrutura do componente dos comentários

---
+++ src/components/Comment.jsx
@@ -0,0 +1,32 @@
+import { ThumbsUp, Trash } from 'phosphor-react'
+import styles from './Comment.module.css'
+
+export function Comment() {
+  return (
+    <div className={styles.comment}>
+      <img src="https://github.com/gabrielmattosl.png"/>
+
+      <div className={styles.commentBox}>
+        <div className={styles.commentContent}>
+          <header>
+            <div className={styles.authorAndTime}>
+              <strong>Gabriel Antônio</strong>
+              <time title='14/01/2025 - 12:33' datetime="2025-01-14 12:33">Cerca de 1h atrás</time>
+            </div>
+
+            <button title='Deletar comentário'>
+              <Trash size={20}/>
+            </button>
+          </header>
+
+          <p>Muito bom, parabéns!!</p>          
+        </div>
+
+        <footer>
+          <ThumbsUp/>
+          Aplaudir <span>20</span>
+        </footer>
+      </div>
+    </div>
+  )
+}
